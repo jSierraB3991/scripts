9c48aa8dc2107dfe9222cdc11843668ca13fb938
Juan David Sierra <eliotandelon@gmail.com>
2022-05-24 02:01:58 -0500

    search worn in the library beep

--- go/notifications/example-beep.go
+++ go/notifications/example-beep.go
@@ -1,16 +1,25 @@
 package main 
 
 import (
+    "os"
+    "os/exec"
     "github.com/gen2brain/beeep"
 )
 
 func main() {
-    err := beeep.Notify("Title", "Message body", "anime/information.png")
+    err := beeep.Notify("Title", "Message body", "dialog-information")
     if err != nil {
         panic(err)
     }
-    err = beeep.Alert("Title", "Message body", "anime/warning.png")
+    err = beeep.Alert("Title", "Message body", "dialog-error")
     if err != nil {
         panic(err)
     }
+
+    send, err := exec.LookPath("notify-send")
+    if err != nil {
+        os.Exit(1)
+    }
+    c := exec.Command(send, "Title", "Message body", "-i", "dialog-information")
+    c.Run()
 }
