5e2fe78c284b0e9dbcf80b9e0f4c7fae2f00ab1b
Juan David Sierra <eliotandelon@gmail.com>
2022-06-14 00:51:53 -0500

    echo go, basic get reoutes

--- go/09-echo-framework/main.go
+++ go/09-echo-framework/main.go
@@ -4,19 +4,48 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hola mundo!")
+}
 
 func main() {
 	port := os.Getenv("APP_PORT")
 	if port == "" {
 		port = "8080"
 	}
+	products := []map[int]string{{1: "Moviles"}, {2: "Tv"}, {3: "Laptops"}}
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hola mundo!")
+
+	e.GET("/", hello)
+	e.GET("/products", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, products)
 	})
+	e.GET("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		pId, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+
+		for _, p := range products {
+			for k := range p {
+				if pId == k {
+					product = p
+				}
+			}
+		}
+
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "Product Not Found")
+		}
+		return c.JSON(http.StatusOK, product)
+	})
+
 	e.Logger.Print(fmt.Sprintf("Listen on port %s", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 }
