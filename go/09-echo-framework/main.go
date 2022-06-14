4456a4121d3ebc1f23f3e2cd2e30a6c5dc4956cb
Juan David Sierra <eliotandelon@gmail.com>
2022-06-14 00:37:49 -0500

    setting env variable

--- go/09-echo-framework/main.go
+++ go/09-echo-framework/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hola mundo!")
 	})
-	e.Logger.Print("Listeng on port 1323")
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Print(fmt.Sprintf("Listen on port %s", port))
+	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 }
