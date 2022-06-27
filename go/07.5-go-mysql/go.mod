7cc5c13c5605442eee76d5d9e140fd57ac7b9e58
Juan David Sierra <eliotandelon@gmail.com>
2022-06-27 16:27:02 -0500

    go connection with mysql

---
+++ go/07.5-go-mysql/go.mod
@@ -0,0 +1,5 @@
+module github.com/OkabeRitarou/07.5-go-mysql
+
+go 1.18
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
--- go/07.5-go-mysql/main.go
+++ go/07.5-go-mysql/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*"))
+
+func dbConnection() *sql.DB {
+	driver := "mysql"
+	user := "root"
+	passwd := "chrrot"
+	dbName := "go_mysql"
+	ipDb := "127.0.0.1"
+
+	conecction, err := sql.Open(driver, user+":"+passwd+"@tcp("+ipDb+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conecction
+}
 
 func Start(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "start", nil)
