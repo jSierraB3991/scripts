99ee781b49d7e45b6bb26ad014b819c3d6715890
Juan David Sierra <eliotandelon@gmail.com>
2022-06-27 16:06:19 -0500

    create employee

--- go/07.5-go-mysql/main.go
+++ go/07.5-go-mysql/main.go
@@ -9,11 +9,16 @@ import (
 var templates = template.Must(template.ParseGlob("templates/*"))
 
 func Start(w http.ResponseWriter, r *http.Request) {
-        templates.ExecuteTemplate(w, "start", nil)
-} 
+	templates.ExecuteTemplate(w, "start", nil)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "create", nil)
+}
 
 func main() {
 	http.HandleFunc("/", Start)
+	http.HandleFunc("/create", Create)
 	log.Println("Server running!!!")
 	http.ListenAndServe(":8080", nil)
 }
