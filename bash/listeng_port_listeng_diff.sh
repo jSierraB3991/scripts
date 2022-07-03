a557a994ba73007491aaaef0f24bafb1dd4ecc5b
Juan David Sierra <eliotandelon@gmail.com>
2022-07-03 13:05:02 -0500

    replace old listeng ports

---
+++ bash/listeng_port_listeng_diff.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+old_ports=$(ss -tulpn | grep LISTEN | awk '{print $5}' | grep "^[1 | 0 | * ]")
+
+function send_notification() {
+    status=$1
+    port=$2
+    echo "notify-send PORT SCAN PORT $port is $status -a PORT_SCAN -i dialog-information"
+    notify-send "PORT SCAN" "PORT $port is $status" -a PORT_SCAN -i dialog-information
+}
+
+while true; do
+    new_ports=$(ss -tulpn | grep LISTEN | awk '{print $5}' | grep "^[1 | 0 | * ]")
+    type_change=$(diff <(echo "$old_ports") <(echo "$new_ports") | head -1)
+    port=$(diff <(echo "$old_ports") <(echo "$new_ports") | tail -1 | awk 'BEGIN{FS=":"} {print $2}' )
+    if [ "$type_change" != "" ] && [ "$port" != "" ]; then
+        echo "type: $type_change port: $port"
+        if [ "$type_change" == "2d1" ]; then
+            send_notification "DOWN" $port
+        elif [ "$type_change" == "1a2" ]  ; then
+            send_notification "UP" $port
+        fi
+    fi
+    old_ports=$new_ports
+    sleep 2
+done
