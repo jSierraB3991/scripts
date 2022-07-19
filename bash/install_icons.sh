9450edc28110212eb5fca92c809978e3fb75ddba
Juan David Sierra <eliotandelon@gmail.com>
2022-07-18 19:47:53 -0500

    fix, rename, and descompress files

--- bash/install_icons.sh
+++ bash/install_icons.sh
@@ -9,7 +9,7 @@ function descompress() {
     7z l $FOLDER/$file 2>/dev/null 1>/dev/null
     if [ "$?" == "0" ]; then
         echo "Descompress file $file"
-        7z x $FOLDER/$file 1>/dev/nul
+        7z x $FOLDER/$file 1>/dev/null
         rm -r $FOLDER/$file
     fi
 
@@ -17,17 +17,39 @@ function descompress() {
 
 function main() {
     for i in $(seq 1 $COUNT_FILES); do
-        file=$(ls $FOLDER | head -$i | tail -1)
-        if [ -f $FOLDER/$file ]; then
-            if [ "$(echo \"$FOLDER/$file\" | grep "\ ")" != ""  ]; then
-                new_name=$(echo $file | sed 's/ /_/g')
-                echo "rename $file to $new_name"
-                mv  "$FOLDER/$file" "$FOLDER/$new_name"
-            fi
-    
-            descompress $file
+    file=$(ls $FOLDER | head -$i | tail -1)
+        if [ "$(echo \"$FOLDER/$file\" | grep "\ ")" != ""  ]; then
+            new_name=$(echo $file | sed 's/ /_/g')
+            echo "rename $file to $new_name"
+            mv  "$FOLDER/$file" "$FOLDER/$new_name"
         fi
     done
+    
+
+    while true; do
+        hasCompressFile=false
+        for element in $(lsd $FOLDER); do
+            7z l $FOLDER/$element 1>/dev/null 2>/dev/null
+            if [ "$?" != "2" ];then
+                hasCompressFile=true
+                break
+            fi
+        done
+
+        if [ "$hasCompressFile" == "true" ]; then
+            for element in $(lsd $FOLDER); do
+                7z l $FOLDER/$element 1>/dev/null 2>/dev/null
+                if [ "$?" != "2" ];then
+                    descompress $element
+                fi
+            done
+        else
+            break
+        fi
+    done
+
+
+    moveToFolderCursorIcons
 }
 
 function moveToFolderCursorIcons() {
@@ -40,21 +62,4 @@ function moveToFolderCursorIcons() {
     done
 }
 
-while true; do
-    hasCompressFile=false
-    for element in $(lsd $FOLDER); do
-        7z l $FOLDER/$element 1>/dev/null 2>/dev/null
-        if [ "$?" != "2" ];then
-            hasCompressFile=true
-            break
-        fi
-    done
-
-    if [ "$hasCompressFile" == "true" ]; then
-        main
-    else
-        break
-    fi
-done
-
-moveToFolderCursorIcons
+main
