136a9356e995c15d71fdb3f1881a88c421f026d6
Juan David Sierra <juan.sierra@zabud.com.co>
2022-08-05 17:11:19 -0500

    verify baterry

--- bash/verify_battery.sh
+++ bash/verify_battery.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
 
+function get_battery() {
+    battery=$1
+    if [[ $battery -le 10 ]]; then
+        echo "10"
+    elif [[ $battery -le 20 ]]; then
+        echo "20"
+    elif [[ $battery -le 30 ]]; then
+        echo "30"
+    elif [[ $battery -le 40 ]]; then
+        echo "40"
+    fi
+}
+
 batstat="$(/usr/bin/cat /sys/class/power_supply/BAT1/status)"
-id_notification=0
 while true; do
     battery="$(/usr/bin/cat /sys/class/power_supply/BAT1/capacity)"
     if [ $batstat == 'Unknown' ]; then
         notify-send "This computer not contains battery, finalizaing script" -u low
         killall verify_battery.sh
-    elif [[ $battery -ge 1 ]] && [[ $battery -le 30 ]]; then
+    elif [[ $battery -ge 1 ]] && [[ $battery -le 40 ]]; then
         batstat="$(/usr/bin/cat /sys/class/power_supply/BAT1/status)"
-        icon="battery-caution"
+        icon="battery-level-$(get_battery $battery)-symbolic.symbolic.png"
         if [ "$batstat" == "Charging" ]; then
-            icon="battery-low-charging"
+            icon="battery-level-$(get_battery $battery)-charging-symbolic.symbolic.png"
         fi
+        icon=$(echo "/usr/share/icons/Adwaita/64x64/status/$icon")
         number=$(cat /sys/class/power_supply/BAT1/capacity)
-
-        new_id=$(notify-send -u critical -i $icon "Battery low percentaje: $number%" -p --replace-id $id_notification)
-        id_notification=$new_id
+        notify-send "Battery Verify" "Battery low percentaje: $number%" -u critical -i $icon -h string:x-canonical-private-synchronous:low_power
     fi
     sleep 1
 done
