d69799cedcc1bbd427eb18fe7a772069fe87ba03
Juan David Sierra <judas3991@gmail.com>
2022-08-23 01:22:54 -0500

    optimize select random wallpaper

--- bash/change_automatic_wallpaper_one.sh
+++ bash/change_automatic_wallpaper_one.sh
@@ -1,16 +1,5 @@
 #! /bin/bash
 
-FOLDER_WALLPAPER="$HOME/Images/background"
+FOLDER_WALLPAPER="$HOME/Source/Wallpapers"
 
-picture_number=$(echo $((1 + $RANDOM % $(ls $FOLDER_WALLPAPER | wc -l) )))
-echo "image number $picture_number"
-image=""
-count=1
-for wallpaper in $(ls $FOLDER_WALLPAPER); do
-    if [ $count == $picture_number ]; then
-        image=$wallpaper
-        break
-    fi
-    let count+=1
-done
-feh --bg-fill $FOLDER_WALLPAPER/$image
+feh --randomize --bg-fill $FOLDER_WALLPAPER/*
