70fb2a00fdb6a1b15ca374fe6b9be6d848add71c
Juan David Sierra <judas3991@gmail.com>
2022-08-23 01:24:08 -0500

    optimize in script for verifing battery

--- bash/verify_battery.sh
+++ bash/verify_battery.sh
@@ -3,31 +3,32 @@
 function get_battery() {
     battery=$1
     if [[ $battery -le 10 ]]; then
-        echo "10"
+        echo "0"
     elif [[ $battery -le 20 ]]; then
-        echo "20"
+        echo "10"
     elif [[ $battery -le 30 ]]; then
-        echo "30"
-    elif [[ $battery -le 40 ]]; then
-        echo "40"
+        echo "20"
+    else
+        echo "100"
     fi
 }
 
 batstat="$(/usr/bin/cat /sys/class/power_supply/BAT1/status)"
+notify_id=0
 while true; do
     battery="$(/usr/bin/cat /sys/class/power_supply/BAT1/capacity)"
     if [ $batstat == 'Unknown' ]; then
         notify-send "This computer not contains battery, finalizaing script" -u low
         killall verify_battery.sh
-    elif [[ $battery -ge 1 ]] && [[ $battery -le 40 ]]; then
+    elif [[ $battery -ge 1 ]] && [[ $battery -le 30 ]]; then
         batstat="$(/usr/bin/cat /sys/class/power_supply/BAT1/status)"
-        icon="battery-level-$(get_battery $battery)-symbolic.symbolic.png"
+        icon="battery-level-$(get_battery $battery)"
         if [ "$batstat" == "Charging" ]; then
-            icon="battery-level-$(get_battery $battery)-charging-symbolic.symbolic.png"
+            icon="battery-level-$(get_battery $battery)-charging"
         fi
-        icon=$(echo "/usr/share/icons/Adwaita/64x64/status/$icon")
+        #icon=$(echo "/usr/share/icons/Adwaita/64x64/status/$icon")
         number=$(cat /sys/class/power_supply/BAT1/capacity)
-        notify-send "Battery Verify" "Battery low percentaje: $number%" -u critical -i $icon -h string:x-canonical-private-synchronous:low_power
+        notify_id=$(notify-send "Battery Verify" "Battery low percentaje: $number%" -u critical -i $icon --replace-id $notify_id -p)
     fi
     sleep 1
 done
