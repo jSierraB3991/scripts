6f919d03ee1a2c01d85233fbd4bf189d1e1d2187
Juan David Sierra <judas3991@gmail.com>
2022-09-28 10:23:49 -0500

    add script to, send notification where have wifi

---
+++ bash/sendNotificationForInternet.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+while true
+do
+    curl -d "Have internet" ntfy.sh/linux_desktop 2>/dev/null
+
+    if [ "$?" != "0" ]
+    then
+        echo "No having Internet"
+    else
+        cowsay "Have internet"
+        break
+    fi
+done
