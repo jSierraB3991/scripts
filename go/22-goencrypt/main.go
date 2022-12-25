393f334a22b82b721778f99ec39b4493de67b25f
Juan David Sierra <eliotandelon@gmail.com>
2022-12-25 13:36:07 -0500

    encrypt by golanf

--- go/22-goencrypt/main.go
+++ go/22-goencrypt/main.go
@@ -1,13 +1,69 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"fmt"
-
-	"github.com/thanhpk/randstr"
 )
 
-func main() {
-	MyString := randstr.String(20)
-	fmt.Println(MyString)
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
 
+// This should be in an env file in production
+const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// Encrypt method is to encrypt or hide any classified text
+func Encrypt(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return Encode(cipherText), nil
+}
+
+func main() {
+	StringToEncrypt := "Contrasena 100 spociento segura no link un mega xD"
+
+	// To encrypt the StringToEncrypt
+	encText, err := Encrypt(StringToEncrypt, MySecret)
+	if err != nil {
+		fmt.Println("error encrypting your classified text: ", err)
+	}
+	fmt.Println(encText)
+
+	decText, err := Decrypt(encText)
+	if err != nil {
+		fmt.Println("error decrypting your encrypted text: ", err)
+	}
+	fmt.Println(decText)
+}
+
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// Decrypt method is to extract back the encrypted text
+func Decrypt(text string) (string, error) {
+
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText), nil
 }
