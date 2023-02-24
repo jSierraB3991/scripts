042efcdefa7e993e18e3512c7a9f3e54457efac8
Juan David Sierra <eliotandelon@gmail.com>
2023-02-24 13:42:00 -0500

    encrypt by tool

--- go/22-goencrypt/main.go
+++ go/22-goencrypt/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"errors"
+	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
-
-// This should be in an env file in production
-const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
 
 func Encode(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
 }
 
+func getInput(reader io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	scanner := bufio.NewScanner(reader)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", nil
+	}
+	text := scanner.Text()
+	if len(text) == 0 {
+		return "", errors.New("Empty message is not allowed")
+	}
+	return text, nil
+}
+
 // Encrypt method is to encrypt or hide any classified text
-func Encrypt(text, MySecret string) (string, error) {
-	block, err := aes.NewCipher([]byte(MySecret))
+func Encrypt(text, secret string) (string, error) {
+	block, err := aes.NewCipher([]byte(secret))
 	if err != nil {
 		return "", err
 	}
@@ -29,21 +48,35 @@ func Encrypt(text, MySecret string) (string, error) {
 	return Encode(cipherText), nil
 }
 
+func Work(secret string, isDecrypt bool, text string) {
+	if !isDecrypt {
+		encText, err := Encrypt(text, secret)
+		if err != nil {
+			fmt.Println("error encrypting your classified text: ", err)
+		}
+		fmt.Println(encText)
+	} else {
+		decText, err := Decrypt(text, secret)
+		if err != nil {
+			fmt.Println("error decrypting your encrypted text: ", err)
+		}
+		fmt.Println(decText)
+	}
+}
+
 func main() {
-	StringToEncrypt := "Contrasena 100 spociento segura no link un mega xD"
 
-	// To encrypt the StringToEncrypt
-	encText, err := Encrypt(StringToEncrypt, MySecret)
-	if err != nil {
-		fmt.Println("error encrypting your classified text: ", err)
-	}
-	fmt.Println(encText)
+	fScret := flag.String("secret", "abc&1*~#^2^#s0^=)^^7%b34", "secret for encrypt and descrypt password")
+	fIsDecrypt := flag.Bool("isDecrypt", false, "Choosee descrypt or encrypt")
+	flag.Parse()
 
-	decText, err := Decrypt(encText)
+	text, err := getInput(os.Stdin, flag.Args()...)
+
 	if err != nil {
-		fmt.Println("error decrypting your encrypted text: ", err)
+		fmt.Println("faile read input: ", err)
 	}
-	fmt.Println(decText)
+	Work(*fScret, *fIsDecrypt, text)
+
 }
 
 func Decode(s string) []byte {
@@ -55,9 +88,9 @@ func Decode(s string) []byte {
 }
 
 // Decrypt method is to extract back the encrypted text
-func Decrypt(text string) (string, error) {
+func Decrypt(text string, secret string) (string, error) {
 
-	block, err := aes.NewCipher([]byte(MySecret))
+	block, err := aes.NewCipher([]byte(secret))
 	if err != nil {
 		return "", err
 	}
