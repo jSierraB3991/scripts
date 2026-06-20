23a362d579d948cbda565d4a67f7ba47ec8c71ac
Juan David Sierra <judas3991@gmail.com>
2026-06-20 10:56:55 -0500

    Add script to remove devices on bluetooth

---
+++ bash/bluetoot_remove.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bluetoothctl disconnect
+
+# Selección
+connect_bluetooth() {
+    device=$(bluetoothctl devices | fzf | awk '{print $2}')
+
+    # Conectar
+    if [ -n "$device" ]; then
+        device_name=$(bluetoothctl devices | grep $device | awk '{print $4}')
+        echo "Remove Device with $device_name device with MAC ADRESS $device"
+        bluetoothctl remove "$device"
+    else
+        echo "No seleccionaste ningún dispositivo"
+    fi
+}
+
+
+connect_bluetooth
