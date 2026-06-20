191eba6ae7782f84571a641ec2e4813757b162c6
Juan David Sierra <judas3991@gmail.com>
2026-06-20 11:54:33 -0500

    basic ls with option -a

--- cpp/ls.c
+++ cpp/ls.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
 
-int main(void) {
-    printf("Hello world. \n");
+int show_all = 0;
+
+int main(int argc, char *argv[]) {
+    int opt;
+
+    while((opt = getopt(argc, argv, "a")) != -1) {
+        switch(opt) {
+            case 'a':
+                show_all = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-a] [path] \n", argv[0]);
+                return 1;
+        }
+    }
+
+    const char *path = (optind > argc) ? argv[optind] : ".";
+    DIR *dir = opendir(path);
+    if(!dir) {
+        perror("Error to open dir");
+        return 1;
+    }
+    struct dirent *entry;
+
+    while((entry = readdir(dir)) != NULL) {
+        if(!show_all && entry->d_name[0] == '.') {
+            continue;
+        }
+        printf("%s \n", entry->d_name);
+    }
+
+    closedir(dir);
+
+
+
     return 0;
 }
