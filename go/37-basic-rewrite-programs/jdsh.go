631a3df1f2d07a6d337f867a11e6ab2964e8479e
Juan David Sierra <judas3991@gmail.com>
2026-07-05 11:03:20 -0500

    add command cd, and run extern external command

--- go/37-basic-rewrite-programs/jdsh.go
+++ go/37-basic-rewrite-programs/jdsh.go
@@ -4,26 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
+func handleCd(args []string) {
+	dir := ""
+	if len(args) == 0 {
+		home, _ := os.UserHomeDir()
+		dir = home
+	} else {
+		dir = args[0]
+	}
+
+	if err := os.Chdir(dir); err != nil {
+		fmt.Printf("cd: No existe el directorio %s\n", dir)
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func runExternal(cmd string, args []string) {
+	command := exec.Command(cmd, args...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		fmt.Printf("jdsh: %s instrucción no encontrada...\n", cmd)
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
 func main() {
+	colorCwd := "\033[33m"
+	resetColor := "\033[0m"
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		cwd, _ := os.Getwd()
-		fmt.Printf("%s $ ", cwd)
+		fmt.Printf("%s %s %s $ ", colorCwd, cwd, resetColor)
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
 		fields := strings.Fields(line)
+		if len(fields) <= 0 {
+			continue
+		}
+
 		cmd := fields[0]
+		args := fields[1:]
 
 		switch cmd {
 		case "exit":
 			os.Exit(0)
+		case "cd":
+			handleCd(args)
+		default:
+			runExternal(cmd, args)
 		}
-
+		fmt.Println()
 	}
 }
