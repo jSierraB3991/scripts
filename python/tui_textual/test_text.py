5f9d89c89149c624089092d4df744bcf463269d2
Juan David Sierra <judas3991@gmail.com>
2026-08-06 23:34:40 -0500

    textual test in python tui

---
+++ python/tui_textual/test_text.py
@@ -0,0 +1,30 @@
+from textual.app import App
+from textual.widgets import Label, Static
+
+class TextApp(App):
+    def compose(self):
+        self.static = Static("I am a [bold red]Static[/bold red] widget!")
+        yield self.static
+        self.label = Label("I am a [yellow italic]Label[/] widget!!")
+        yield self.label
+
+    def on_mount(self):
+        self.static.styles.background = "blue"
+        self.static.styles.border = ("solid", "white")
+        self.static.styles.text_aling = "center"
+        self.static.styles.padding = 1,1
+        self.static.styles.margin = 4,4
+        
+        self.label.styles.background = "darkgreen"
+        self.label.styles.border = ("double", "red")
+        self.label.styles.padding = 1,1
+        self.label.styles.margin = 2,4
+
+    def on_key(self, event):
+        match event.key:
+            case q:
+                exit()
+
+if __name__ == "__main__":
+    app = TextApp()
+    app.run()
